cdda002fdfccc71008db5af37239d5c8c1c070f5
Natalia Stiopu <stiopuaurora@gmail.com>
2026-07-17 08:19:19 +0000

    proyecto corregido

--- src/models.py
+++ src/models.py
@@ -18,56 +18,45 @@ class User(db.Model):
 
     favorites_characters: Mapped[List["FavoriteCharacter"]] = relationship(
         back_populates="user")
-    
+
     favorites_planets: Mapped[List["FavoritePlanet"]] = relationship(
         back_populates="user")
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
-
-
-class Characters(db.Model):
+class Character(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(nullable=False)
     hair_color: Mapped[str] = mapped_column(nullable=False)
     eye_color: Mapped[str] = mapped_column(nullable=False)
     birth_year: Mapped[int] = mapped_column(nullable=False)
-
     favorites_characters: Mapped[List["FavoriteCharacter"]] = relationship(
-        back_populates="characters")
+        back_populates="character")
 
 
 class FavoriteCharacter(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-
-    characters: Mapped["Characters"] = relationship(
+    character: Mapped["Character"] = relationship(
         back_populates="favorites_characters")
     character_id: Mapped[int] = mapped_column(
-        ForeignKey("characters.id"), nullable=False)
-
-    users: Mapped["User"] = relationship(back_populates="favorites_characters")
-    users_id: Mapped[int] = mapped_column(
+        ForeignKey("character.id"), nullable=False)
+    user: Mapped["User"] = relationship(back_populates="favorites_characters")
+    user_id: Mapped[int] = mapped_column(
         ForeignKey("user.id"), nullable=False)
 
 
-class Planets(db.Model):
+class Planet(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(nullable=False)
     climate: Mapped[str] = mapped_column(nullable=False)
     gravity: Mapped[str] = mapped_column(nullable=False)
     population: Mapped[int] = mapped_column(nullable=False)
-
     favorites_planets: Mapped[List["FavoritePlanet"]] = relationship(
-        back_populates="planets")
-
+        back_populates="planet")
 
 class FavoritePlanet(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    planets: Mapped["Planets"] = relationship(back_populates="favorites_planets")
-    planet_id: Mapped[int] = mapped_column(ForeignKey("planets.id"), nullable=False)
-    users: Mapped["User"] = relationship(back_populates="favorites_planets")
-    users_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    planet: Mapped["Planet"] = relationship(
+        back_populates="favorites_planets")
+    planet_id: Mapped[int] = mapped_column(
+        ForeignKey("planet.id"), nullable=False)
+    user: Mapped["User"] = relationship(back_populates="favorites_planets")
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
